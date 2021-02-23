d830cfb5fed41e61f38cf3113106345e31aa78ea
dongjae-kim <dongjae.k.kim@gmail.com>
2021-02-23 16:39:53 +0900

    upload

---
+++ whole_brain_analysis/contrast_06_RPE_BL/dfaas.m
@@ -0,0 +1,11 @@
+%-----------------------------------------------------------------------
+% Job saved on 10-Sep-2019 14:31:47 by cfg_util (rev $Rev: 6460 $)
+% spm SPM - SPM12 (6906)
+% cfg_basicio BasicIO - Unknown
+%-----------------------------------------------------------------------
+matlabbatch{1}.spm.stats.fmri_est.spmmat = '<UNDEFINED>';
+matlabbatch{1}.spm.stats.fmri_est.write_residuals = 0;
+matlabbatch{1}.spm.stats.fmri_est.method.Classical = 1;
+matlabbatch{2}.spm.stats.con.spmmat = '<UNDEFINED>';
+matlabbatch{2}.spm.stats.con.consess = {};
+matlabbatch{2}.spm.stats.con.delete = 0;
